b22ca92623be8dfbcc06ad18d83c2159ed57350b
Daniel McMahon <daniel40392@gmail.com>
2018-01-15 20:10:55 +0000

    added props.children to Person component

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
+//note: class-based components use this.props
+
 const person = (props) => {
   return (
-    <p>I'm a {props.name} and I am {props.age} years old!</p>
+    <div>
+      <p>I'm a {props.name} and I am {props.age} years old!</p>
+      <p>{props.children}</p> {/* refers to elements between component tags */}
+    </div>
   )
 }
 
