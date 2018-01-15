fd76df83dceb503025eeedbeefead7a62373e59d
Daniel McMahon <daniel40392@gmail.com>
2018-01-15 20:03:41 +0000

    added dynamic JS to component

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //       <h2>Hi I'm a React App!</h2>
-    //   </div>
-    // );
+    return (
+      <div className="App">
+          <h2>Hi I'm a React App!</h2>
+          <p>This is really working!</p>
+          <Person />
+          <Person />
+          <Person />
+      </div>
+    );
 
-    return React.createElement('div', {className: 'App'}, React.createElement('h1',null, 'DOES THIS WORK NOW?' ))
+    //return React.createElement('div', {className: 'App'}, React.createElement('h1',null, 'DOES THIS WORK NOW?' ))
   }
 }
 
