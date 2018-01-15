f5f60b11224b7bd075167a6d42aa3d8b3ddee83a
Daniel McMahon <daniel40392@gmail.com>
2018-01-15 20:30:31 +0000

    added some setState functionality

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 //note: class-based components use this.props
+//note: function based components cannot use state
 
 const person = (props) => {
   return (
     <div>
-      <p>I'm a {props.name} and I am {props.age} years old!</p>
+      <p>I'm {props.name} and I am {props.age} years old!</p>
       <p>{props.children}</p> {/* refers to elements between component tags */}
     </div>
   )
