b6f1baf459cf53804d28abc11bdbe7952413e306
Daniel McMahon <daniel40392@gmail.com>
2018-01-22 19:46:58 +0000

    updated errors and improved lists

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -5,35 +5,39 @@ import Person from "./Person/Person";
 class App extends Component {
   state = {
     persons: [
-      { name: "Max", age: 28 },
-      { name: "Manu", age: 29 },
-      { name: "Stephanie", age: 26 }
+      { id: 'asdf', name: "Max", age: 28 },
+      { id: 'sdfg', name: "Manu", age: 29 },
+      { id: 'dfgh', name: "Stephanie", age: 26 }
     ],
     otherState: "some other value",
     showPersons: false
   };
 
-  switchNameHandler = newName => {
-    // console.log('Was clicked!')
-    // DON'T DO THIS this.state.persons[0].name = 'Maximilian';
+  nameChangedHandler = ( event, id ) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    }); //built in JS method
 
-    this.setState({
-      persons: [
-        { name: newName, age: 28 },
-        { name: "Manu", age: 29 },
-        { name: "Stephanie", age: 27 }
-      ]
-    });
+    //const person = this.state.persons[personIndex]; //careful of mutation - reference
+    //const person = Object.assign({}, this.state.persons[personIndex]) //this works
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({persons: persons });
   };
 
-  nameChangedHandler = event => {
-    this.setState({
-      persons: [
-        { name: "Max", age: 28 },
-        { name: event.target.value, age: 29 },
-        { name: "Stephanie", age: 26 }
-      ]
-    });
+  deletePersonHandler = (personIndex) => {
+    //const persons = this.state.persons; //bad practice due to being a pointer to setState
+    //const persons = this.state.persons.slice(); //returns a copy of the original this would be ok
+    const persons = [...this.state.persons]; //spreads state into a new array - also would be ok
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
   };
 
   togglePersonHandler = () => {
@@ -55,8 +59,16 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          {this.state.persons.map(person => {
-            return <Person name={person.name} age={person.age}/>
+          {this.state.persons.map((person, index) => {
+            return (
+              <Person
+                click={() => this.deletePersonHandler(index)}
+                name={person.name}
+                age={person.age}
+                key={person.id}
+                changed={(event) => this.nameChangedHandler(event, person.id)}
+              />
+            );
           })}
         </div>
       );
